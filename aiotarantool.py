bb100cebf302c0bf57c59e790595b28767b353ea
Dzianis Pirshtuk <d_pirshtuk@indatalabs.com>
2015-12-02 20:25:25 +0000

    trollius usage

--- aiotarantool.py
+++ aiotarantool.py
@@ -2,7 +2,9 @@
 
 __version__ = "1.0.5"
 
-import asyncio
+import trollius as asyncio
+from trollius import From, Return, Future
+
 import socket
 import errno
 import msgpack
@@ -62,22 +64,22 @@ class Schema(object):
     @asyncio.coroutine
     def get_space(self, space):
         try:
-            return self.schema[space]
+            raise Return(self.schema[space])
         except KeyError:
             pass
 
         if not self.con.connected:
-            yield from self.con.connect()
+            yield From(self.con.connect())
 
-        with (yield from self.con.lock):
+        with (yield From(self.con.lock)):
             if space in self.schema:
-                return self.schema[space]
+                raise Return(self.schema[space])
 
             _index = (tarantool.const.INDEX_SPACE_NAME
                       if isinstance(space, str)
                       else tarantool.const.INDEX_SPACE_PRIMARY)
 
-            array = yield from self.con.select(tarantool.const.SPACE_SPACE, space, index=_index)
+            array = yield From(self.con.select(tarantool.const.SPACE_SPACE, space, index=_index))
             if len(array) > 1:
                 raise SchemaError('Some strange output from server: \n' + array)
 
@@ -87,28 +89,28 @@ class Schema(object):
                     "There's no space with {1} '{0}'".format(space, temp_name))
 
             array = array[0]
-            return SchemaSpace(array, self.schema)
+            raise Return(SchemaSpace(array, self.schema))
 
     @asyncio.coroutine
     def get_index(self, space, index):
-        _space = yield from self.get_space(space)
+        _space = yield From(self.get_space(space))
         try:
-            return _space.indexes[index]
+            raise Return(_space.indexes[index])
         except KeyError:
             pass
 
         if not self.con.connected:
-            yield from self.con.connect()
+            yield From(self.con.connect())
 
-        with (yield from self.con.lock):
+        with (yield From(self.con.lock)):
             if index in _space.indexes:
-                return _space.indexes[index]
+                raise Return(_space.indexes[index])
 
             _index = (tarantool.const.INDEX_INDEX_NAME
                       if isinstance(index, str)
                       else tarantool.const.INDEX_INDEX_PRIMARY)
 
-            array = yield from self.con.select(tarantool.const.SPACE_INDEX, [_space.sid, index], index=_index)
+            array = yield From(self.con.select(tarantool.const.SPACE_INDEX, [_space.sid, index], index=_index))
 
             if len(array) > 1:
                 raise SchemaError('Some strange output from server: \n' + array)
@@ -120,7 +122,7 @@ class Schema(object):
                         index, _space.name, temp_name))
 
             array = array[0]
-            return SchemaIndex(array, _space)
+            raise Return(SchemaIndex(array, _space))
 
     def flush(self):
         self.schema.clear()
@@ -165,14 +167,14 @@ class Connection(tarantool.Connection):
     @asyncio.coroutine
     def connect(self):
         if self.connected:
-            return
+            raise Return()
 
-        with (yield from self.lock):
+        with (yield From(self.lock)):
             if self.connected:
-                return
+                raise Return()
 
             logger.log(logging.DEBUG, "connecting to %r" % self)
-            self._reader, self._writer = yield from asyncio.open_connection(self.host, self.port, loop=self.loop)
+            self._reader, self._writer = yield From(asyncio.open_connection(self.host, self.port, loop=self.loop))
             self.connected = True
 
             self._reader_task = self.loop.create_task(self._response_reader())
@@ -183,13 +185,13 @@ class Connection(tarantool.Connection):
             self._auth_event = asyncio.Event(loop=self.loop)
 
         if self.user and self.password:
-            yield from self._auth_event.wait()
-            yield from self.authenticate(self.user, self.password)
+            yield From(self._auth_event.wait())
+            yield From(self.authenticate(self.user, self.password))
 
     @asyncio.coroutine
     def _response_writer(self):
         while True:
-            yield from self._write_event.wait()
+            yield From(self._write_event.wait())
 
             if self._write_buf:
                 to_write = self._write_buf
@@ -201,16 +203,16 @@ class Connection(tarantool.Connection):
     @asyncio.coroutine
     def _response_reader(self):
         # handshake
-        greeting = yield from self._reader.read(IPROTO_GREETING_SIZE)
+        greeting = yield From(self._reader.read(IPROTO_GREETING_SIZE))
         self._salt = base64.decodestring(greeting[64:])[:20]
         self._auth_event.set()
 
         buf = b""
         while not self._reader.at_eof():
-            tmp_buf = yield from self._reader.read(self.aiobuffer_size)
+            tmp_buf = yield From(self._reader.read(self.aiobuffer_size))
             if not tmp_buf:
-                yield from self._do_close(
-                    NetworkError(socket.error(errno.ECONNRESET, "Lost connection to server during query")))
+                yield From(self._do_close(
+                    NetworkError(socket.error(errno.ECONNRESET, "Lost connection to server during query"))))
 
             buf += tmp_buf
             len_buf = len(buf)
@@ -245,21 +247,21 @@ class Connection(tarantool.Connection):
             if curr:
                 buf = buf[curr:]
 
-        yield from self._do_close(None)
+        yield From(self._do_close(None))
 
     @asyncio.coroutine
     def _wait_response(self, sync):
-        resp = yield from self._waiters[sync]
+        resp = yield From(self._waiters[sync])
         # renew request waiter
         self._waiters[sync] = asyncio.Future(loop=self.loop)
-        return resp
+        raise Return(resp)
 
     @asyncio.coroutine
     def _send_request(self, request):
         assert isinstance(request, Request)
 
         if not self.connected:
-            yield from self.connect()
+            yield From(self.connect())
 
         sync = request.sync
         for attempt in range(RETRY_MAX_ATTEMPTS):
@@ -270,9 +272,9 @@ class Connection(tarantool.Connection):
             self._write_event.set()
 
             # read response
-            response = yield from waiter
+            response = yield From(waiter)
             if response.completion_status != 1:
-                return response
+                raise Return(response)
 
             self._waiters[sync] = asyncio.Future(loop=self.loop)
             warn(response.return_message, RetryWarning)
@@ -290,13 +292,13 @@ class Connection(tarantool.Connection):
 
     @asyncio.coroutine
     def close(self):
-        yield from self._do_close(None)
+        yield From(self._do_close(None))
 
     def _do_close(self, exc):
         if not self.connected:
             return
 
-        with (yield from self.lock):
+        with (yield From(self.lock)):
             self.connected = False
             self._writer.transport.close()
             self._reader_task.cancel()
@@ -330,8 +332,8 @@ class Connection(tarantool.Connection):
         if len(args) == 1 and isinstance(args[0], (list, tuple)):
             args = args[0]
 
-        resp = yield from self._send_request(RequestCall(self, func_name, args))
-        return resp
+        resp = yield From(self._send_request(RequestCall(self, func_name, args)))
+        raise Return(resp)
 
     @asyncio.coroutine
     def eval(self, expr, *args):
@@ -340,17 +342,17 @@ class Connection(tarantool.Connection):
         if len(args) == 1 and isinstance(args[0], (list, tuple)):
             args = args[0]
 
-        resp = yield from self._send_request(RequestEval(self, expr, args))
-        return resp
+        resp = yield From(self._send_request(RequestEval(self, expr, args)))
+        raise Return(resp)
 
     @asyncio.coroutine
     def replace(self, space_name, values):
         if isinstance(space_name, str):
-            sp = yield from self.schema.get_space(space_name)
+            sp = yield From(self.schema.get_space(space_name))
             space_name = sp.sid
 
-        resp = yield from self._send_request(RequestReplace(self, space_name, values))
-        return resp
+        resp = yield From(self._send_request(RequestReplace(self, space_name, values)))
+        raise Return(resp)
 
     @asyncio.coroutine
     def authenticate(self, user, password):
@@ -358,48 +360,48 @@ class Connection(tarantool.Connection):
         self.password = password
 
         if not self.connected:
-            yield from self.connect()
+            yield From(self.connect())
 
-        resp = yield from self._send_request(RequestAuthenticate(self, self._salt, self.user, self.password))
-        return resp
+        resp = yield From(self._send_request(RequestAuthenticate(self, self._salt, self.user, self.password)))
+        raise Return(resp)
 
     @asyncio.coroutine
     def join(self, server_uuid):
         request = RequestJoin(self, server_uuid)
-        resp = yield from self._send_request(request)
+        resp = yield From(self._send_request(request))
         while True:
             yield resp
             if resp.code == REQUEST_TYPE_OK or resp.code >= REQUEST_TYPE_ERROR:
-                return
+                raise Return()
 
-            resp = yield from self._wait_response(request.sync)
+            resp = yield From(self._wait_response(request.sync))
 
         # close connection after JOIN
-        yield from self.close()
+        yield From(self.close())
 
     @asyncio.coroutine
     def subscribe(self, cluster_uuid, server_uuid, vclock=None):
         vclock = vclock or dict()
         request = RequestSubscribe(self, cluster_uuid, server_uuid, vclock)
-        resp = yield from self._send_request(request)
+        resp = yield From(self._send_request(request))
         while True:
             yield resp
             if resp.code >= REQUEST_TYPE_ERROR:
-                return
+                raise Return()
 
-            resp = yield from self._wait_response(request.sync)
+            resp = yield From(self._wait_response(request.sync))
 
         # close connection after SUBSCRIBE
-        yield from self.close()
+        yield From(self.close())
 
     @asyncio.coroutine
     def insert(self, space_name, values):
         if isinstance(space_name, str):
-            sp = yield from self.schema.get_space(space_name)
+            sp = yield From(self.schema.get_space(space_name))
             space_name = sp.sid
 
-        res = yield from self._send_request(RequestInsert(self, space_name, values))
-        return res
+        res = yield From(self._send_request(RequestInsert(self, space_name, values)))
+        raise Return(res)
 
     @asyncio.coroutine
     def select(self, space_name, key=None, **kwargs):
@@ -411,17 +413,17 @@ class Connection(tarantool.Connection):
         key = check_key(key, select=True)
 
         if isinstance(space_name, str):
-            sp = yield from self.schema.get_space(space_name)
+            sp = yield From(self.schema.get_space(space_name))
             space_name = sp.sid
 
         if isinstance(index_name, str):
-            idx = yield from self.schema.get_index(space_name, index_name)
+            idx = yield From(self.schema.get_index(space_name, index_name))
             index_name = idx.iid
 
-        res = yield from self._send_request(
-            RequestSelect(self, space_name, index_name, key, offset, limit, iterator_type))
+        res = yield From(self._send_request(
+            RequestSelect(self, space_name, index_name, key, offset, limit, iterator_type)))
 
-        return res
+        raise Return(res)
 
     @asyncio.coroutine
     def delete(self, space_name, key, **kwargs):
@@ -429,17 +431,17 @@ class Connection(tarantool.Connection):
 
         key = check_key(key)
         if isinstance(space_name, str):
-            sp = yield from self.schema.get_space(space_name)
+            sp = yield From(self.schema.get_space(space_name))
             space_name = sp.sid
 
         if isinstance(index_name, str):
-            idx = yield from self.schema.get_index(space_name, index_name)
+            idx = yield From(self.schema.get_index(space_name, index_name))
             index_name = idx.iid
 
-        res = yield from self._send_request(
-            RequestDelete(self, space_name, index_name, key))
+        res = yield From(self._send_request(
+            RequestDelete(self, space_name, index_name, key)))
 
-        return res
+        raise Return(res)
 
     @asyncio.coroutine
     def update(self, space_name, key, op_list, **kwargs):
@@ -447,27 +449,27 @@ class Connection(tarantool.Connection):
 
         key = check_key(key)
         if isinstance(space_name, str):
-            sp = yield from self.schema.get_space(space_name)
+            sp = yield From(self.schema.get_space(space_name))
             space_name = sp.sid
 
         if isinstance(index_name, str):
-            idx = yield from self.schema.get_index(space_name, index_name)
+            idx = yield From(self.schema.get_index(space_name, index_name))
             index_name = idx.iid
 
-        res = yield from self._send_request(
-            RequestUpdate(self, space_name, index_name, key, op_list))
+        res = yield From(self._send_request(
+            RequestUpdate(self, space_name, index_name, key, op_list)))
 
-        return res
+        raise Return(res)
 
     @asyncio.coroutine
     def ping(self, notime=False):
         request = RequestPing(self)
         t0 = self.loop.time()
-        yield from self._send_request(request)
+        yield From(self._send_request(request))
         t1 = self.loop.time()
 
         if notime:
-            return "Success"
+            raise Return("Success")
 
-        return t1 - t0
+        raise Return(t1 - t0)
 
